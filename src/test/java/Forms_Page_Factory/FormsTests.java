83a178f716a6742c170384ac5cf3e340abb7fa7c
Dimitri777 <dimitri.golubev1978@gmail.com>
2024-09-23 21:15:18 +0300

    Updated FormTests.java and PracticeForm.java with completed loginToPracticeForm() and practiceFormTest()

--- src/test/java/Forms_Page_Factory/FormsTests.java
+++ src/test/java/Forms_Page_Factory/FormsTests.java
@@ -1,93 +1,80 @@
 package Forms_Page_Factory;
 
-import Forms_Page_Factory.LoginForm;
-import Forms_Page_Factory.RegisterForm;
-
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
-import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
-
-import java.time.Duration;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
 
 public class FormsTests {
 
     WebDriver driver;
-    WebDriverWait wait;
     String loginUrl = "https://www.tutorialspoint.com/selenium/practice/login.php";
     String registerUrl = "https://www.tutorialspoint.com/selenium/practice/register.php";
+    String practiceUrl = "https://www.tutorialspoint.com/selenium/practice/selenium_automation_practice.php";
+
     @Before
     public void setUp() {
         System.setProperty("webdriver.gecko.driver", "C:\\Users\\mrx\\Documents\\Drivers\\firefox\\geckodriver\\geckodriver.exe");
-
         driver = new FirefoxDriver();
-
+        driver.manage().window().maximize(); // Открытие браузера в полноэкранном режиме
     }
 
     @Test
     public void registerUserTest() {
-        // Открытие веб-страницы
-        String registerUrl = "https://www.tutorialspoint.com/selenium/practice/register.php#";
         driver.get(registerUrl);
-        driver.manage().window().maximize();
-
         RegisterForm regform = new RegisterForm(driver);
-        regform.register("Dimitri", "Golubev", "dimitri777", "privet");
+        regform.register("Dimitri", "Golubev", "dimitri7773", "privet");
 
-        assertEquals(driver.getCurrentUrl(), registerUrl);
+        assertEquals(driver.getCurrentUrl(), registerUrl+"#");
+
+    }
+
+    @Test
+    public void practiceFormTest() {
+        String pageTitle = "Selenium Practice - Student Registration Form";
+        driver.get(practiceUrl);
+        PracticeForm practiceForm = new PracticeForm(driver);
+        practiceForm.loginToPracticeForm("Alexandra Ivanova", "aliv@gmail.com", "+74951234567",
+                "01.01.1980", "Math, Physics", "Test address whatever, 1, 2-3/5");
+
+        assertEquals(driver.getCurrentUrl(), practiceUrl);
+        assertEquals(driver.getTitle(), pageTitle);
     }
 
     @Test
     public void checkLoginTest() {
-        // Открытие веб-страницы
-
         driver.get(loginUrl);
-        driver.manage().window().maximize();
-
         LoginForm loginForm = new LoginForm(driver);
         loginForm.login("dimitri777", "privet");
 
         assertEquals(driver.getCurrentUrl(), loginUrl);
-
     }
 
     @Test
-    public void backToLoginFormfromRegisterFormTest(){
-        // Открытие веб-страницы
+    public void backToLoginFormfromRegisterFormTest() {
         driver.get(registerUrl);
-        driver.manage().window().maximize();
-
         RegisterForm regform = new RegisterForm(driver);
         regform.backToLoginForm();
 
-        System.out.println(driver.getCurrentUrl());
         assertEquals(driver.getCurrentUrl(), loginUrl);
     }
 
     @Test
-    public void toRegistrationFormFromLoginFormTest(){
-        // Открытие веб-страницы
+    public void toRegistrationFormFromLoginFormTest() {
         driver.get(loginUrl);
-        driver.manage().window().maximize();
-
         LoginForm loginform = new LoginForm(driver);
         loginform.goToRegistrationForm();
 
-        //System.out.println(driver.getCurrentUrl());
         assertEquals(driver.getCurrentUrl(), registerUrl);
     }
+
     @After
     public void tearDown() {
         if (driver != null) {
-            driver.quit();
+          //  driver.quit();
         }
     }
 }
--- src/test/java/Forms_Page_Factory/PracticeForm.java
+++ src/test/java/Forms_Page_Factory/PracticeForm.java
@@ -1,7 +1,102 @@
 package Forms_Page_Factory;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+
 public class PracticeForm {
 
+    WebDriver driver;
+
+    @FindBy(id = "name")
+    WebElement nameField;
+
+    @FindBy(id = "email")
+    WebElement emailField;
+
+    @FindBy(id = "gender")
+    WebElement maleRadioButton;
+
+    @FindBy(xpath = "//*[@id=\"practiceForm\"]/div[3]/div/div/div[2]/input")
+    WebElement femaleRadioButton;
+
+    @FindBy(xpath = "//*[@id=\"practiceForm\"]/div[3]/div/div/div[3]/input")
+    WebElement otherRadioButton;
+
+    @FindBy(id = "mobile")
+    WebElement mobileField;
+
+    @FindBy(xpath = "//*[@id=\"dob\"]")
+    WebElement dobField;
+
+    @FindBy(id = "subjects")
+    WebElement subjectsField;
+
+    @FindBy(id = "hobbies")
+    WebElement sportsCheckbox;
+
+    @FindBy(xpath = "//*[@id=\"practiceForm\"]/div[7]/div/div/div[2]/input")
+    WebElement readingCheckbox;
+
+    @FindBy(xpath = "//*[@id=\"practiceForm\"]/div[7]/div/div/div[3]/input")
+    WebElement musicCheckbox;
+
+    @FindBy(xpath = "//input[@id='picture']")
+    WebElement pictureChooseFileBox;
+
+    @FindBy(xpath = "//textarea[@id='picture']")
+    WebElement currentAddressTextArea;
+
+    @FindBy(id = "state")
+    WebElement stateListbox;
+
+    @FindBy(id = "city")
+    WebElement cityListbox;
+
+    @FindBy(xpath = "//*[@id=\"practiceForm\"]/div[11]/input")
+    WebElement submitButton;
+
+    // Constructor
+    public PracticeForm(WebDriver driver) {
+        this.driver = driver;
+        PageFactory.initElements(driver, this);
+    }
+
+    public void loginToPracticeForm(String name, String email, String mobileNumber, String dateOfBirth, String subjects, String currAddress) {
+
+        nameField.sendKeys(name);
+        emailField.sendKeys(email);
+        femaleRadioButton.click();
+        otherRadioButton.click();
+        maleRadioButton.click();
+
+        mobileField.sendKeys(mobileNumber);
+
+        dobField.sendKeys(dateOfBirth);
+
+        subjectsField.sendKeys(subjects);
+
+        sportsCheckbox.click();
+        readingCheckbox.click();
+        musicCheckbox.click();
 
 
+        pictureChooseFileBox.click();
+        File file = new File("C:\\Users\\mrx\\Desktop\\1.jpg");
+        pictureChooseFileBox.sendKeys(file.getAbsolutePath());
+
+        currentAddressTextArea.sendKeys(currAddress);
+
+        Select selectState = new Select(stateListbox);
+        selectState.selectByVisibleText("Haryana");
+
+        Select selectCity = new Select(cityListbox);
+        selectCity.selectByVisibleText("Agra");
+
+        submitButton.click();
+    }
 }
